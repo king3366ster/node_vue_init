fd8894c35fab93fcf5c14978cfc2cec02c9c63cb
king3366ster <king3366ster@gmail.com>
2017-07-15 20:04:10 +0800

    compatetive with node 6

--- examples/node-koa/backend/server.js
+++ examples/node-koa/backend/server.js
@@ -3,26 +3,26 @@ const path = require('path')
 const fse = require('fs-extra')
 
 // node7+ only
-// import session from 'koa-session'
-// import bodyParser from 'koa-bodyparser'
-// import mount from 'koa-mount'
-// import serve from 'koa-static'
-// import send from 'koa-send'
+import session from 'koa-session'
+import bodyParser from 'koa-bodyparser'
+import mount from 'koa-mount'
+import serve from 'koa-static'
+import send from 'koa-send'
 
 const app = new Koa()
 
 // static
-// app.use(mount('/dist', serve(path.join(__dirname, '../webapp/dist'))))
+app.use(mount('/dist', serve(path.join(__dirname, '../webapp/dist'))))
 
 // session
-// app.keys = ['koa test']
-// app.use(session(app, {
-//   // 默认会话结束后 cookie 失效
-//   // maxAge: 24 * 60 * 60 * 1000
-// }))
+app.keys = ['koa test']
+app.use(session(app, {
+  // 默认会话结束后 cookie 失效
+  // maxAge: 24 * 60 * 60 * 1000
+}))
 
 // body parser
-// app.use(bodyParser())
+app.use(bodyParser())
 
 // for all get requests, return index.html
 app.use(async (ctx, next) => {
